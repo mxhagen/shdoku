46f470821b0cd9cba05f5539102a0a0583258e41
mxhagen <markichnich@proton.me>
2024-09-24 18:40:02 +0200

    fix validation of 3x3 blocks

--- src/sudoku/validator.rs
+++ src/sudoku/validator.rs
@@ -22,7 +22,7 @@ pub fn is_solution(sudoku: &Board) -> bool {
         let mut block_set = 0u16;
         for j in 0..9 {
             let tmp = block_set;
-            block_set ^= 1 << sudoku[i / 3 * 3 + i % 3][j / 3 * 3 + j % 3];
+            block_set ^= 1 << sudoku[i / 3 * 3 + j / 3][i % 3 * 3 + j % 3];
             if block_set < tmp {
                 return false;
             }
